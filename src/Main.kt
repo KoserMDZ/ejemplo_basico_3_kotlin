1fd3d75690166552d571c2f588c4c76f27a60a3f
David Jardón <david.jardon@optivamedia.com>
2021-07-23 14:13:35 +0200

    Random

--- src/Main.kt
+++ src/Main.kt
@@ -7,7 +7,7 @@ fun main() {
 }
 
 fun ifUsage(){
-    val numero = 0
+    val numero = Random.nextInt(0,12)
     // If - Else Clasico
     if (numero == 0) {
         println("$numero es igual a 0 ")
@@ -23,13 +23,13 @@ fun ifUsage(){
 }
 
 fun whenUsageWithParameters(){
-    val numero = 0
+    val numero = Random.nextInt(-10,103)
 
     when (numero) {
         0 -> println("$numero es igual a 0 ")
-        in 1..99 -> println("$numero es un numero comprendido entre 0 y 9")
+        in 1..99 -> println("$numero es un numero comprendido entre 0 y 99")
         100, 101 -> println("$numero es 100 o 101")
-        else -> println("$numero es menor de 0 y mayor de 101")
+        else -> println("$numero es menor de 0 o mayor de 101")
     }
 }
 
